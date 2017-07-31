d6f2129869f71bdd2ecbaeca7af100aafae3349c
Satoshi Scott Iwako <iwakoscott@gmail.com>
2017-07-31 12:33:28 -0700

    finish query one and part one.

--- scripts/main.py
+++ scripts/main.py
@@ -5,17 +5,28 @@
 
 import psycopg2
 
-def query_one():
-    sql = '''SELECT path, count(*) AS n FROM log
-            WHERE path like '/article/%'
-            GROUP BY path
-            ORDER BY n DESC
-            LIMIT 3;'''
-            
+query_one = '''SELECT title FROM articles JOIN
+        (SELECT path, count(*) AS n FROM log
+        WHERE path like '/article/%'
+        GROUP BY path
+        ORDER BY n DESC
+        LIMIT 3) as n_views
+        ON n_views.path ILIKE '%' || articles.slug || '%'
+        ORDER BY n_views.n DESC;'''
+
+def part_one():
+    with psycopg2.connect('dbname=news') as conn:
+        cur = conn.cursor()
+        cur.execute(query_one)
+        data = [x[0] for x in cur.fetchall()]
+        return data
+
+
 def main():
     #TODO: 1. What are the most popular three articles of all time? Which
     #articles have been accessed the most? Present this information as a sorted
     #list with the most popular article at the top.
+    print(part_one())
 
     #TODO: 2. Who are the most popular article authors of all time? That is,
     #when you sum up all of the articles each author has written, which authors
