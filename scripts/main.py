4ead5adbfb87bad2b024595d106e8ffe24bdbdc4
Satoshi Scott Iwako <iwakoscott@gmail.com>
2017-07-31 11:11:41 -0700

    add 3 todos to give project some direction/

--- scripts/main.py
+++ scripts/main.py
@@ -4,3 +4,22 @@
 # Udacity SQL Project
 
 import psycopg2
+
+def main():
+    #TODO: 1. What are the most popular three articles of all time? Which
+    #articles have been accessed the most? Present this information as a sorted
+    #list with the most popular article at the top.
+
+    #TODO: 2. Who are the most popular article authors of all time? That is,
+    #when you sum up all of the articles each author has written, which authors
+    #get the most page views? Present this as a sorted list with the most
+    #popular author at the top.
+
+    #TODO: 3. On which days did more than 1% of requests lead to errors?
+    #The log table includes a column status that indicates the HTTP status code
+    #that the news site sent to the user's browser.
+
+
+
+if __name__ == '__main__':
+    main()
