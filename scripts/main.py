a2fc0d41f2edd7f8acf17f6213db42af05d73ae5
Satoshi Scott Iwako <iwakoscott@gmail.com>
2017-08-01 10:35:03 -0700

    add query two and complete part_two function.

--- scripts/main.py
+++ scripts/main.py
@@ -5,7 +5,8 @@
 
 import psycopg2
 
-query_one = '''SELECT title FROM articles JOIN
+query_one = '''
+        SELECT title FROM articles JOIN
         (SELECT path, count(*) AS n FROM log
         WHERE path like '/article/%'
         GROUP BY path
@@ -14,24 +15,42 @@ query_one = '''SELECT title FROM articles JOIN
         ON n_views.path ILIKE '%' || articles.slug || '%'
         ORDER BY n_views.n DESC;'''
 
+query_two = '''
+        SELECT articlesauthors.name, COUNT(*) AS n FROM (SELECT * FROM articles
+        JOIN authors
+        ON articles.author=authors.id) AS articlesauthors
+        JOIN log ON log.path ILIKE '%' || articlesauthors.slug || '%'
+        GROUP BY articlesauthors.name
+        ORDER BY n DESC;'''
+
 def part_one():
+    """
+    1. What are the most popular three articles of all time? Which
+    #articles have been accessed the most? Present this information as a sorted
+    #list with the most popular article at the top.
+    """
     with psycopg2.connect('dbname=news') as conn:
         cur = conn.cursor()
         cur.execute(query_one)
         data = [x[0] for x in cur.fetchall()]
         return data
 
+def part_two():
+    """
+    TODO: 2. Who are the most popular article authors of all time? That is,
+    when you sum up all of the articles each author has written, which authors
+    get the most page views? Present this as a sorted list with the most
+    popular author at the top.
+    """
+    with psycopg2.connect('dbname=news') as conn:
+        cur = conn.cursor()
+        cur.execute(query_two)
+        data = [x[0] for x in cur.fetchall()]
+        return data
 
 def main():
-    #TODO: 1. What are the most popular three articles of all time? Which
-    #articles have been accessed the most? Present this information as a sorted
-    #list with the most popular article at the top.
     print(part_one())
-
-    #TODO: 2. Who are the most popular article authors of all time? That is,
-    #when you sum up all of the articles each author has written, which authors
-    #get the most page views? Present this as a sorted list with the most
-    #popular author at the top.
+    print(part_two())
 
     #TODO: 3. On which days did more than 1% of requests lead to errors?
     #The log table includes a column status that indicates the HTTP status code
